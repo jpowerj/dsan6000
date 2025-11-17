984a527754f2451213316de28473d35bd7e53f72
Jeff Jacobs <jjacobs3@cs.stanford.edu>
2025-11-17 15:12:57 -0500

    images updated

--- w10/images/Thomas Sankara was right_ _He who... - The African Dream _ Facebook_files/IvuZV3jE-JC.js
+++
@@ -1,3 +0,0 @@
-;/*FB_PKG_DELIM*/
-
-__d("isSoftNewlineEvent",["Keys"],(function(t,n,r,o,a,i){"use strict";function e(e){return e.which===n("Keys").RETURN&&(e.getModifierState("Shift")||e.getModifierState("Alt")||e.getModifierState("Control"))}a.exports=e}),null);
